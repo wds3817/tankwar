78b30ace68182d0bbc8437fcfd1ad18010f46638
wds3817 <wds3817@gmail.com>
2020-06-24 02:43:36 +1000

    敌方坦克团灭，就地复活

--- src/main/java/com/javawds/tankwar/Missile.java
+++ src/main/java/com/javawds/tankwar/Missile.java
@@ -16,6 +16,15 @@ public class Missile {
 
     private final boolean enemy;
     private final Direction direction;
+    private boolean live = true;
+
+    public boolean isLive() {
+        return live;
+    }
+
+    public void setLive(boolean live) {
+        this.live = live;
+    }
 
     Image getImage() {
         return direction.getImage("missile");
@@ -69,8 +78,46 @@ public class Missile {
     void draw(Graphics g) {
         move();
         if (x < 0 || x > 800 || y < 0 || y > 600) {
+            this.live = false;
+            //GameClient.getInstance().removeMissile(this);
             return;
         }
+        Rectangle rectangle = this.getRectangle();
+
+        for (Wall wall : GameClient.getInstance().getWalls()) {
+            if (rectangle.intersects(wall.getRectangle())) {
+                this.setLive(false);
+                //GameClient.getInstance().removeMissile(this);
+                return;
+            }
+        }
+
+        if (enemy) {
+            Tank playerTank = GameClient.getInstance().getPlayerTank();
+            if (rectangle.intersects(playerTank.getRectangle())) {
+                playerTank.setHp(playerTank.getHp() - 20);
+                if (playerTank.getHp() <= 0) {
+                    playerTank.setLive(false);
+                }
+                this.setLive(false);
+                //GameClient.getInstance().removeMissile(this);
+            }
+        } else {
+            for (Tank tank : GameClient.getInstance().getEnemyTanks()) {
+                if (rectangle.intersects(tank.getRectangle())) {
+                    tank.setLive(false);
+                    this.setLive(false);
+                    //GameClient.getInstance().removeMissile(this);
+                    break;
+                }
+            }
+        }
+
+
         g.drawImage(getImage(), x, y, null);
+    }
+
+    Rectangle getRectangle() {
+        return new Rectangle(x, y, getImage().getWidth(null), getImage().getHeight(null));
     }
 }
--- src/main/java/com/javawds/tankwar/Tank.java
+++ src/main/java/com/javawds/tankwar/Tank.java
@@ -15,7 +15,28 @@ class Tank {
     private int y;
     private Direction direction;
     private boolean enemy;
+    private boolean live = true;
+    private int hp = 100;
 
+     int getHp() {
+        return hp;
+    }
+
+     void setHp(int hp) {
+        this.hp = hp;
+    }
+
+    boolean isLive() {
+        return live;
+    }
+
+    void setLive(boolean live) {
+        this.live = live;
+    }
+
+    boolean isEnemy() {
+        return enemy;
+    }
 
      Tank(int x, int y, Direction direction) {
         this(x, y, false, direction);
@@ -82,6 +103,9 @@ class Tank {
     }
 
     void draw(Graphics g) {
+         if (!this.live) {
+
+         }
         int oldx = x, oldy = y;
         this.determineDirection();
         this.move();
@@ -116,7 +140,7 @@ class Tank {
         g.drawImage(this.getImage(), this.x, this.y, null);
     }
 
-     private Rectangle getRectangle() {
+    Rectangle getRectangle() {
         return new Rectangle(x, y, getImage().getWidth(null), getImage().getHeight(null));
     }
 
@@ -214,4 +238,5 @@ class Tank {
         }
         //this.determineDirection();
     }
+
 }
